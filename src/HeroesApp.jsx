831b7cec77cbff5197e01cdadfe68610858d5cac
smog <abdielorellana3@gmail.com>
2021-07-25 11:18:45 -0400

    add components for heroes app ans assets

---
+++ src/HeroesApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const HeroesApp = () => {
+    return (
+        <div>
+            <h1>Heroes app coponent</h1>
+            <br/>
+            
+        </div>
+    )
+}
