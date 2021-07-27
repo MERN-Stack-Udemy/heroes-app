07ccd13fb139efbd852341640b35a79a98873286
smog <abdielorellana3@gmail.com>
2021-07-26 21:34:58 -0400

    implimentes useMemo into HeroesList

--- src/components/heroes/HeroesList.jsx
+++ src/components/heroes/HeroesList.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { getHeroByPublisher } from '../../selectors/getHeroByPublisher'
 import { HeroCard } from './HeroCard';
 
 export const HeroesList = ({ publisher }) => {
 
-    const heroes = getHeroByPublisher(publisher);
+    const heroes = useMemo(() => getHeroByPublisher(publisher), [publisher]);
+    // const heroes = getHeroByPublisher(publisher);
     return (
         <div className="card-columns">
            {
