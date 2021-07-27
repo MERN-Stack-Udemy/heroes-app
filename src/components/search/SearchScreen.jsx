275c2ae9a0d24171b91b183a06eb9847d98c25b0
smog <abdielorellana3@gmail.com>
2021-07-27 17:14:24 -0400

    implements search component into navbar and useForm for form search

--- src/components/search/SearchScreen.jsx
+++ src/components/search/SearchScreen.jsx
@@ -1,19 +1,35 @@
 import React from 'react'
+import { heroes } from '../../data/heroes'
+import { useForm } from '../../Hooks/useForm';
+import { HeroCard } from '../heroes/HeroCard';
 
 export const SearchScreen = () => {
+
+    const [{search}, handleChange, reset] = useForm({search:''});
+    const heroesFiltered = heroes;
+    const handleSearch = (e) => {
+        e.preventDefault();
+
+        console.log(search);
+        reset();
+    }
+    
     return (
         <div>
-            <h1>Serar Screen</h1>
+            <h1>Search Screen</h1>
             <hr />
             <div className="row">
                 <div className="col-5">
                     <h4 > Search Form</h4>
                     <hr />
-                    <form action="">
-                        <input 
+                    <form onSubmit={ handleSearch }>
+                        <input
+                            name="search"
                             type="text" 
                             placeholder="Find your hero"
                             className="form-control"
+                            onChange={handleChange}
+                            value={search}
                         />
                         <button
                             type="submit"
@@ -22,6 +38,18 @@ export const SearchScreen = () => {
                             Search
                         </button>
                     </form>
+                </div>
+                <div className="col-7">
+                    <h4> Resulsts</h4>
+                    <hr />
+                    {
+                        heroesFiltered.map( hero => (
+                            <HeroCard
+                                key={hero.id}
+                                {...hero}
+                            />
+                        ))
+                    }
                 </div>
 
             </div>
