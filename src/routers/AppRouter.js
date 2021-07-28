e261bb2d056170b5c911f76a8342d5430c8cab72
smog <abdielorellana3@gmail.com>
2021-07-28 02:54:41 -0400

    implememt PublicRoute for LoginScreen public routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,9 +1,10 @@
 import React, { useContext } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
 import { AuthContext } from '../auth/AuthContext';
 import { LoginScreen } from '../components/login/LoginScreen';
 import { DashboardRoutes } from './DashboardRoutes';
 import { PrivateRoutes } from './PrivateRoutes';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
 
@@ -14,12 +15,19 @@ export const AppRouter = () => {
         <Router>
             <div>
                 <Switch>
-                    <Route exact path="/login" component={ LoginScreen } />
+
+                    <PublicRoute 
+                        exact path="/login" 
+                        component={ LoginScreen } 
+                        isAuthenticated={user.logged}
+                    />
+
                     <PrivateRoutes 
                         path="/" 
                         component={ DashboardRoutes } 
                         isAuthenticated={user.logged}
                     />
+
                 </Switch>
             </div>
         </Router>
