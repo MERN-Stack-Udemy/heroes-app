5a15c5f35f9751a6b7e4aa4f5fe7cc1021817293
smog <abdielorellana3@gmail.com>
2021-07-28 18:41:10 -0400

    fix test configuration and test path location

--- src/setupTests.js
+++ src/setupTests.js
@@ -8,4 +8,4 @@ import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import { createSerializer } from 'enzyme-to-json';
 
 Enzyme.configure({ adapter: new Adapter() });
-expect.addSnapshotSerializer( createSerializer({mode: 'deep'}) );
+expect.addSnapshotSerializer( createSerializer({mode:'deep'}) )
---
+++ src/tests/auth/authReducer.test.js
@@ -0,0 +1,15 @@
+
+describe('test on authReducer', () => {
+    
+    test('should return default state', () => {
+        
+    })
+
+    test('should autenticate and put user name', () => {
+        
+    })
+    
+    test('should logout and delete user', () => {
+        
+    })
+})
